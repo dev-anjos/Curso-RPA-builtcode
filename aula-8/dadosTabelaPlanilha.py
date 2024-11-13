6f0710b185b61078f8a5d4a0079e3cb9d1c5a4aa
Juan Anjos <82560036+dev-anjos@users.noreply.github.com>
2024-11-13 19:03:16 -0300

    Add files via upload

--- aula-8/dadosTabelaPlanilha.py
+++ aula-8/dadosTabelaPlanilha.py
@@ -8,17 +8,18 @@ navegador = se.Chrome()
 navegador.get("https://rpachallengeocr.azurewebsites.net/")
 sleep(2)
 dataFrame = pandas
-dataFrameLista = []
 
+dataFrameLista = []
 i = 1
 while i < 4:
+
 
     # linha = 1
 
     elementoTabela = navegador.find_element(By.XPATH, '//*[@id="tableSandbox"]/tbody')
 
     linhas = elementoTabela.find_elements(By.TAG_NAME, 'tr')
-    colunas = elementoTabela.find_elements(By.TAG_NAME, 'td')
+    # colunas = elementoTabela.find_elements(By.TAG_NAME, 'td')
 
     i = i + 1
 
@@ -33,8 +34,6 @@ while i < 4:
     dataFrame.to_excel(arquivoExcel, sheet_name='sheel', index=False)
     arquivoExcel._save()
 
-    navegador.find_element(By.XPATH, '//*[@id="tableSandbox_next"]').click()
-
     sleep(3)
 
 
@@ -42,5 +41,5 @@ while i < 4:
 
 
 
-
+  navegador.find_element(By.XPATH, '//*[@id="tableSandbox_next"]').click()
 
