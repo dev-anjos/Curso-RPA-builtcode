839149a4b8c18e1e9c4f8ef6d6009845b7258950
Juan Anjos <82560036+dev-anjos@users.noreply.github.com>
2024-12-11 18:58:31 -0300

    Add files via upload

--- projeto-final/contracoes.py
+++ projeto-final/contracoes.py
@@ -31,69 +31,96 @@ import pandas
 scrollToElement = "arguments[0].scrollIntoView({ block: 'center' });"
 
 navegador = se.Chrome()
-navegador.get("https://pncp.gov.br/app/editais?q=&status=recebendo_proposta&pagina=1")
-wordKeys = 'Dipirona sódica'
 
-search = navegador.find_element(By.ID, 'keyword')
-search.send_keys(wordKeys)
+wordKeys = ["Dipirona Sódica", "Clonazepam", "Atenalol"]
 
-sleep(2)
+for keys in wordKeys:
 
-checkBox = navegador.find_element(By.XPATH, '//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-top-panel/div/div/div[1]/div[2]/div/div[4]/div/label' )
-checkBox.click()
+    navegador.get("https://pncp.gov.br/app/editais?q=&status=recebendo_proposta&pagina=1")
 
-sleep(5)
+    search = navegador.find_element(By.ID, 'keyword')
 
-searchBtn = navegador.find_element(By.XPATH, '//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-top-panel/div/div/div[3]/div[2]/button')
-# searchBtn = '//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-top-panel/div/div/div[3]/div[2]/button'
-
-
-navegador.execute_script(scrollToElement, searchBtn)
-# scroll_to_element(navegador,searchBtn)
-
-sleep(5)
-searchBtn.click()
-
-sleep(5)
-
-frameEditals = navegador.find_element(By.XPATH, '//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-tab[1]/div/div[2]/div/div[2]/pncp-items-list')
-links = []
-for x in frameEditals.find_elements(By.XPATH, '//*[@title= "Acessar item."]'):
-    link = x.get_attribute("href")
-    links.append(link)
-
-for link in links:
-    navegador.get(link)
-    sleep(10)
-    try:
-        buttonDetail = WebDriverWait(navegador, 50).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@class="br-button circle ng-star-inserted"]'))
-      )
-    except Exception as e:
-        print("Erro ao carrega detalhes do edital")
-
-    print(f"abriu o site: {link}")
-    buttonDetail = navegador.find_elements(By.XPATH, '//*[@class="br-button circle ng-star-inserted"]')
-    tableItens = navegador.find_element(By.XPATH, '//*[@title="Itens"]')
-    # itens = navegador.find_element
-    for btn in buttonDetail:
-        sleep(5)
-        navegador.execute_script(scrollToElement, tableItens)
-        btn.click()
-        ##aqui pegará os dados
-        sleep(2)
-        btnReturn = navegador.find_element(By.XPATH, '//*[@class="br-button primary small m-2"]')
-  
-        btnReturn.click()
-        sleep(2)
-
+    search.send_keys(keys)
     sleep(2)
-   
 
-    
+    checkBox = (
+        navegador.find_element(By.XPATH,'//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-top-panel/div/div/div[1]/div[2]/div/div[4]/div/label'))
+
+    checkBox.click()
+
+    sleep(5)
+
+    searchBtn = (
+        navegador.find_element(By.XPATH, '//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-top-panel/div/div/div[3]/div[2]/button'))
+
+    navegador.execute_script(scrollToElement, searchBtn)
+
+    sleep(5)
+    searchBtn.click()
+
+    sleep(5)
+
+    frameEditals = (
+        navegador.find_element(By.XPATH,'//*[@id="main-content"]/pncp-list/pncp-results-panel/pncp-tab-set/div/pncp-tab[1]/div/div[2]/div/div[2]/pncp-items-list'))
 
 
-print(links)
-# sleep(5)
+    numberOfResults = navegador.find_element(By.XPATH, './/*[@id="tam_pagina"]')
+    navegador.execute_script(scrollToElement, numberOfResults)
+    numberOfResults.click()
+    cemResults = numberOfResults.find_element(By.XPATH, './/*[@title="100"]')
+    cemResults.click()
+
+    links = []
+    text = []
+
+    for x in frameEditals.find_elements(By.XPATH, '//*[@title= "Acessar item."]'):
+        link = x.get_attribute("href")
+        links.append(link)
+
+    # btnNext = navegador.find_element(By.XPATH, './/*[@data-next-page="data-next-page"]')
+    # btnNext.click()
+
+    for link in links:
+        navegador.get(link)
+        sleep(10)
+        try:
+            buttonDetail = WebDriverWait(navegador, 50).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@class="br-button circle ng-star-inserted"]'))
+            )
+        except Exception as e:
+            print("Erro ao carrega detalhes do edital")
+
+        print(f"abriu o site: {link}")
+
+        buttonDetail = navegador.find_elements(By.XPATH, '//*[@class="br-button circle ng-star-inserted"]')
+        sleep(2)
+
+        # tableItens = navegador.find_element(By.XPATH, '//*[@title="Itens"]')
+        for btn in buttonDetail:
+            sleep(5)
+            navegador.execute_script(scrollToElement, btn)
+            btn.click()
+            ##aqui pegará os dados
+            modalDetail = navegador.find_element(By.XPATH, '//*[@class="br-card"]')
+            # textItem = modalDetail.find_elements(By.TAG_NAME, "span")
+
+            for modal in modalDetail:
+                span = modal.find_elements(By.TAG_NAME, "span")
+                text.append(span.text)
 
 
+            print(text)
+            sleep(2)
+            btnReturn = navegador.find_element(By.XPATH, '//*[@class="br-button primary small m-2"]')
+
+            btnReturn.click()
+            sleep(2)
+
+            sleep(2)
+
+    print(links)
+
+
+
+
+sleep(5)
